736d856a5a7b747fd987ed9c537f37eb670de07d
zxy518 <2016995502@qq.com>
2025-01-16 17:36:37 +0800

    Delete 哎哟不错哟 directory

--- 哎哟不错哟/scripts/main.js
+++
@@ -1,6 +0,0 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "欢迎来到我的网页";
-document.querySelector("html").addEventListener("click", function () {
-    alert("别戳我，我怕疼！");
-  });
-document.querySelsector("")  
